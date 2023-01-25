3fd5c471e132e1d5431c38238b3d8716ee13948e
Aakash <aakash.codes02@gmail.com>
2023-01-25 13:56:00 +0530

    implicit types with arrays and objects

--- 01-Type basics/sandbox.js
+++ 01-Type basics/sandbox.js
@@ -1,3 +1,6 @@
+/*====
+====== String Number & Boolean Types ======
+                                     ====*/
 var character = 'mario';
 var age = 30;
 var isBlackBelt = false;
@@ -11,3 +14,33 @@ var circ = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circ(7.5));
+/*====
+====== Object & Array Types ======
+                            ====*/
+// Arrays
+var names = ['luigi', 'adam', 'eve'];
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
+var numbers = [12, 15, 18];
+numbers.push(25);
+// numbers[1] = 'shaun';
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 2;
+// Objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+ninja = {
+    name: 'yohsi',
+    belt: 'orange',
+    age: 40
+};
--- 01-Type basics/sandbox.ts
+++ 01-Type basics/sandbox.ts
@@ -1,3 +1,7 @@
+/*====
+====== String Number & Boolean Types ======
+                                     ====*/
+
 let character = 'mario';
 let age = 30;
 let isBlackBelt = false;
@@ -16,3 +20,40 @@ const circ = (diameter: number) => {
 };
 
 console.log(circ(7.5));
+
+/*====
+====== Object & Array Types ======
+                            ====*/
+
+// Arrays
+let names = ['luigi', 'adam', 'eve'];
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
+
+let numbers = [12, 15, 18];
+numbers.push(25);
+// numbers[1] = 'shaun';
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 2;
+
+// Objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30,
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+
+ninja = {
+  name: 'yohsi',
+  belt: 'orange',
+  age: 40,
+};
